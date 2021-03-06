0c123cf9465095e06737be5e25408bf865eb766e
Jingchun Wang <inginx747@gmail.com>
2021-03-05 23:05:22 -0800

    added license

--- buffer.h
+++ buffer.h
@@ -1,3 +1,26 @@
+// ======================================================================== //
+// Copyright (c) 2021 Ingram Inxent                                         //
+//                                                                          //
+// Permission is hereby granted, free of charge, to any person obtaining    //
+// a copy of this software and associated documentation files (the          //
+// "Software"), to deal in the Software without restriction, including      //
+// without limitation the rights to use, copy, modify, merge, publish,      //
+// distribute, sublicense, and/or sell copies of the Software, and to       //
+// permit persons to whom the Software is furnished to do so, subject to    //
+// the following conditions:                                                //
+//                                                                          //
+// The above copyright notice and this permission notice shall be           //
+// included in all copies or substantial portions of the Software.          //
+//                                                                          //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          //
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF       //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                    //
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE   //
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION   //
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION    //
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.          //
+// ======================================================================== //
+
 #pragma once
 #ifndef BUFFER_H
 #define BUFFER_H
@@ -44,6 +67,11 @@ public:
     inline size_t stride() const { return mStride; }
 
     inline bool empty() const { return mSize == 0; }
+
+    inline void set_stride(size_t stride) { if (mStride == 1) mStride = stride; }
+
+    template<class T_Derived>
+    inline void set_stride() { if (mStride == 1) mStride = sizeof(T_Derived); }
 
     // Requests a change in capacity
     // reserve() will never decrase the capacity.
